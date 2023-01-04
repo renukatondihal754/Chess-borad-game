ade87b9673c9276fd862a8d97879ffc6610e969c
renukatondihal754 <111186672+renukatondihal754@users.noreply.github.com>
2023-01-04 11:56:02 +0530

    Add files via upload
    Coins are added

---
+++ coin.py
@@ -0,0 +1,8 @@
+#print(__name__)
+class Coin:
+    def __init__(self,x,y):
+        self.pos=(x,y)
+
+if __name__=='__main__':     #e file run madiga matra edu run agutte import madidaga run agalla
+    c = Coin(20,30) #__name__ spcial variable 
+    print(c.pos)
